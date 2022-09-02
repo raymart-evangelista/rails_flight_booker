c394eba45bb2dfdb73e8798597a1e9b5e9d937d4
raymart-evangelista <raymart.s.evangelista@gmail.com>
2022-09-01 19:52:46 -0700

    add Airport and Flight seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,10 +6,6 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
-Airport.destroy_all
-Flight.destroy_all
-
-
 Airport.create!([{
   code: "SFO"
 },
@@ -31,6 +27,29 @@ Airport.create!([{
 
 p "Created #{Airport.count} airports"
 
-# Flight.create!([{
-#   departure_id: 
-# }])
+Flight.create!([{
+  departure_id: 1,
+  arrival_id: 2,
+  start_datetime: DateTime.civil(2022, 9, 19, 6, 30),
+  duration: 1.hour+35.minutes
+},
+{
+  departure_id: 1,
+  arrival_id: 3,
+  start_datetime: DateTime.civil(2022, 9, 20, 7, 30),
+  duration: 5.hour+40.minutes
+},
+{
+  departure_id: 4,
+  arrival_id: 5,
+  start_datetime: DateTime.civil(2022, 9, 21, 8, 30),
+  duration: 55.minutes
+},
+{
+  departure_id: 6,
+  arrival_id: 3,
+  start_datetime: DateTime.civil(2022, 9, 22, 9, 30),
+  duration: 5.hours+35.minutes
+}])
+
+p "Created #{Flight.count} flights"
