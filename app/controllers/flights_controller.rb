aa9df903f5c8707f0ee47dd71796d22cf566881b
raymart-evangelista <raymart.s.evangelista@gmail.com>
2022-09-01 23:46:32 -0700

    add flight search form to index view

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,5 +1,14 @@
 class FlightsController < ApplicationController
   def index
     @flights = Flight.all
+    # @airport_options = Airport.all.map { |a| [a.code, a.id] }
+    # @flight_dates = Flight.distinct.pluck(:start_datetime)
+    # @flight_dates = Flight.all.order("start_datetime").distinct.pluck(:start_datetime)
+    # @flight_dates = Flight.all.map { |f| [f.start_datetime, f.id] }
+
+  end
+
+  def new
+    @flight = Flight.new
   end
 end
