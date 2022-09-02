f08d68b855b261a07aa7823b342ba4e45e13c3fe
raymart-evangelista <raymart.s.evangelista@gmail.com>
2022-09-02 00:05:18 -0700

    switch select_date for select_tag

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,11 +1,15 @@
 class FlightsController < ApplicationController
   def index
     @flights = Flight.all
-    # @airport_options = Airport.all.map { |a| [a.code, a.id] }
+    @airport_options = Airport.all.map { |a| [a.code] }
+    # @airport_options = Airport.all.map { |a| [a.code] }
+    # @airport_options = Airport.distinct.pluck(:code)
+    @year_options = Flight.all.map { |f| [f.start_datetime.year] }.uniq
+    @day_options = Flight.all.map { |f| [f.start_datetime.day] }.uniq
+    @month_options = Flight.all.map { |f| [f.start_datetime.month] }.uniq
     # @flight_dates = Flight.distinct.pluck(:start_datetime)
     # @flight_dates = Flight.all.order("start_datetime").distinct.pluck(:start_datetime)
     # @flight_dates = Flight.all.map { |f| [f.start_datetime, f.id] }
-
   end
 
   def new
