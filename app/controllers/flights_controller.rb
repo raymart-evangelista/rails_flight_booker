47bbd0e6330ce8013c8458f9e3e2b849a05ab148
raymart-evangelista <raymart.s.evangelista@gmail.com>
2022-09-02 00:32:10 -0700

    move code from controller to view, add some logic to flights controller

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,15 +1,13 @@
 class FlightsController < ApplicationController
   def index
-    @flights = Flight.all
-    @airport_options = Airport.all.map { |a| [a.code] }
-    # @airport_options = Airport.all.map { |a| [a.code] }
-    # @airport_options = Airport.distinct.pluck(:code)
-    @year_options = Flight.all.map { |f| [f.start_datetime.year] }.uniq
-    @day_options = Flight.all.map { |f| [f.start_datetime.day] }.uniq
-    @month_options = Flight.all.map { |f| [f.start_datetime.month] }.uniq
-    # @flight_dates = Flight.distinct.pluck(:start_datetime)
-    # @flight_dates = Flight.all.order("start_datetime").distinct.pluck(:start_datetime)
-    # @flight_dates = Flight.all.map { |f| [f.start_datetime, f.id] }
+    # @flights = Flight.all
+    if params.key?(:month) && params.key?(:day) && params.key?(:year)
+      if params[:departure_code] == params[:arrival_code]
+        flash.now[:alert] = "Departure airport and arrival airport cannot be the same."
+      else
+        @selected_date = Date.civil(params[:year].to_i, params[:month].to_i, params[:day].to_i)
+      end
+    end
   end
 
   def new
