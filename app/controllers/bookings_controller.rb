7b22a9c8bbbf0356a3add00c873e756bc3d73167
raymart-evangelista <raymart.s.evangelista@gmail.com>
2022-09-05 22:33:56 -0700

    receive params in BookingsController, add variables to bookings#new

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -3,9 +3,6 @@ class BookingsController < ApplicationController
     # receive flight_id and passenger number parameters
     @flight_id = params[:flight_id]
     @passenger_amt = params[:passenger_amt]
-
-    flash.now[:alert] = "flight_id: #{@flight_id}, passenger_amt: #{@passenger_amt}"
-    # @booking = Booking.new
   end
 
   def create
