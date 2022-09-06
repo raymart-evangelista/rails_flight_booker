3b96037b750517bb97d5d25e4ef0b7f772603a79
raymart-evangelista <raymart.s.evangelista@gmail.com>
2022-09-06 15:40:23 -0700

    fix associations between passenger, flight, and booking models

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -3,7 +3,7 @@ class Flight < ApplicationRecord
   belongs_to :arriving_airport, primary_key: :id, foreign_key: :arrival_id, class_name: 'Airport'
 
   has_many :bookings
-  has_many :passengers
+  has_many :passengers, :through => :bookings
 
   def flight_path_with_details
     "#{departing_airport.code} -> #{arriving_airport.code}, #{start_datetime.strftime("%Y-%m-%d %H:%M %p")}, #{duration.inspect}"
