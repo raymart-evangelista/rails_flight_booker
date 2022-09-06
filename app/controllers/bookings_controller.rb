a49fc51622b605079789f7604590b6966791163c
raymart-evangelista <raymart.s.evangelista@gmail.com>
2022-09-06 15:23:38 -0700

    add booking_params and passenger fields form

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -3,9 +3,18 @@ class BookingsController < ApplicationController
     # receive flight_id and passenger number parameters
     @flight_id = params[:flight_id]
     @passenger_amt = params[:passenger_amt]
+
+    @booking = Booking.new
   end
 
   def create
+    @booking = Booking.new(booking_params)
+  end
 
+  private
+  
+  def booking_params
+    params.require(:booking).permit(:flight_id, :passenger_amt,
+      :passenger_attributes => [:id, :name, :email])
   end
 end
