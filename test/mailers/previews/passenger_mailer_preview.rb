9b58fb32e2b9021fc98711da82aad1149444f5f9
raymart-evangelista <raymart.s.evangelista@gmail.com>
2022-09-13 19:43:16 -0700

    add email preview

--- test/mailers/previews/passenger_mailer_preview.rb
+++ test/mailers/previews/passenger_mailer_preview.rb
@@ -1,4 +1,6 @@
 # Preview all emails at http://localhost:3000/rails/mailers/passenger_mailer
 class PassengerMailerPreview < ActionMailer::Preview
-
+  def confirmation_email
+    PassengerMailer.with(passenger: Passenger.first).confirmation_email
+  end
 end
